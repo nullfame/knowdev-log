6e71b07a32049a39fdb023811f857f3cd22a4e4a
Adam Finlayson <amf@adamfinlayson.com>
2021-08-06 23:27:41 -0500

    feat: util: stringify

---
+++ src/util/stringify.js
@@ -0,0 +1,36 @@
+const formatAsJsonString = require("./formatAsJsonString");
+
+//
+//
+// Main
+//
+
+const stringify = (...params) => {
+  if (params.length === 0) return "";
+  if (params.length === 1) {
+    return formatAsJsonString(params[0]);
+  }
+
+  // If none of the params are objects, return a space-separated string
+  const noObjects = params.reduce((previous, current) => {
+    if (typeof current === "object" && current !== null) {
+      return false;
+    }
+    return previous;
+  }, true);
+
+  if (noObjects) {
+    const formatted = params.map(formatAsJsonString);
+    return formatted.join(" ");
+  }
+
+  // Okay, something is an object, just formatAsJsonString
+  return formatAsJsonString(params);
+};
+
+//
+//
+// Export
+//
+
+module.exports = stringify;
