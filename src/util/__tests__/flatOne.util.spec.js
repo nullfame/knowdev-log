0a39c239cd88add01caf74dff0b5f2ce2d034acf
Adam Finlayson <amf@adamfinlayson.com>
2023-10-13 19:59:16 -0500

    feat: flat one function logic

--- src/util/__tests__/flatOne.util.spec.js
+++ src/util/__tests__/flatOne.util.spec.js
@@ -32,4 +32,43 @@ describe("FlatOne util", () => {
   it("Is a function", () => {
     expect(flatOne).toBeFunction();
   });
+  it("Returns undefined if nothing is passed", () => {
+    expect(flatOne()).toBeUndefined();
+  });
+  it("If the value is not an array, it returns the value", () => {
+    // Arrange
+    const value = "value";
+    // Act
+    const result = flatOne(value);
+    // Assert
+    expect(result).toBe(value);
+    // Done
+  });
+  it("If the value is an array with one item, it returns that item", () => {
+    // Arrange
+    const value = ["value"];
+    // Act
+    const result = flatOne(value);
+    // Assert
+    expect(result).toBe(value[0]);
+    // Done
+  });
+  it("If the value is an array with zeros item, it returns the empty array", () => {
+    // Arrange
+    const value = [];
+    // Act
+    const result = flatOne(value);
+    // Assert
+    expect(result).toBe(value);
+    // Done
+  });
+  it("If the value is an array with multiple items, it returns the array", () => {
+    // Arrange
+    const value = ["value", "value"];
+    // Act
+    const result = flatOne(value);
+    // Assert
+    expect(result).toBe(value);
+    // Done
+  });
 });
--- src/util/flatOne.util.js
+++ src/util/flatOne.util.js
@@ -13,8 +13,16 @@
 // Main
 //
 
-const flatOne = () => {
-  //
+/** Flattens an array of one to just that value */
+const flatOne = (value) => {
+  // If the value is not an array, return the value
+  if (!Array.isArray(value)) return value;
+
+  // If the value is an array with one item, return that item
+  if (value.length === 1) return value[0];
+
+  // Return the array
+  return value;
 };
 
 //
